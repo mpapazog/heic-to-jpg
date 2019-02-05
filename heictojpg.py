215a8309dc881f7422018cc5bbdce2711220eda7
mpapazog <spamandviruses@gmail.com>
2019-02-05 16:34:21 +0200

    Add files via upload

--- heictojpg.py
+++ heictojpg.py
@@ -1,6 +1,12 @@
-import os
+#HEIC to JPG image format batch conversion script for Python 3. Tested on Windows 10.
+#You will need to have ImageMagick installed: https://www.imagemagick.org/
+
+import os, subprocess
+
+directory = '.'
 
 for filename in os.listdir(directory):
     if filename.lower().endswith(".heic"): 
-        print(os.path.join(directory, filename))
+        print('Converting %s...' % os.path.join(directory, filename))
+        subprocess.run(["magick", "%s" % filename, "%s" % (filename[0:-5] + '.jpg')])
         continue
