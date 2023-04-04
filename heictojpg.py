ec2942c2493d4dcc8f9548e23d2ab7686c789d3a
Garrett Broters <52166834+Garr-R@users.noreply.github.com>
2023-04-04 16:40:14 -0400

    Added HEIF file support
    Just a quick elif statement in order to support .HEIF files as well

--- heictojpg.py
+++ heictojpg.py
@@ -10,3 +10,7 @@ for filename in os.listdir(directory):
         print('Converting %s...' % os.path.join(directory, filename))
         subprocess.run(["magick", "%s" % filename, "%s" % (filename[0:-5] + '.jpg')])
         continue
+    elif filename.lower().endswith(".heif"):
+        print('Converting %s...' % os.path.join(directory, filename))
+        subprocess.run(["magick", "%s" % filename, "%s" % (filename[0:-5] + '.jpg')])
+        continue
